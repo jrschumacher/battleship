7a35a4985c7c581048a3d0161b73e27ee5cff2de
Ryan Schumacher <ryan@38pages.com>
2018-06-13 18:59:52 -0500

    Update cli help

--- src/cli.js
+++ src/cli.js
@@ -10,15 +10,6 @@ const Ui = importJsx('./components/ui');
 const cli = meow(`
 	Usage
 	  $ battleship
-
-	Options
-	  --emoji [Default: true]
-
-	Examples
-	  $ battleship
-	  Welcome to Battleship 🌊⚓️💣 [Press Enter]
-	  $ jrscumacher-battleship-ink --emoji=false
-	  Welcome to Battleship [Press Enter]
 `);
 
 render(h(Ui, cli.flags));
