d20196d8c197b609889a572202c9d5da120bf6e5
Ryan Schumacher <ryan@38pages.com>
2018-06-13 19:36:12 -0500

    Uncomment multiple ships

--- src/Battleship.js
+++ src/Battleship.js
@@ -17,22 +17,22 @@ import {
 
 export const BATTLESHIP_BOARD_SIZE = 10;
 export const BATTLESHIP_FLEET = [
-  // {
-  //   name: "Carrier",
-  //   size: 5
-  // },
-  // {
-  //   name: "Battleship",
-  //   size: 4
-  // },
-  // {
-  //   name: "Cruiser",
-  //   size: 3
-  // },
-  // {
-  //   name: "Submarine",
-  //   size: 3
-  // },
+  {
+    name: "Carrier",
+    size: 5
+  },
+  {
+    name: "Battleship",
+    size: 4
+  },
+  {
+    name: "Cruiser",
+    size: 3
+  },
+  {
+    name: "Submarine",
+    size: 3
+  },
   {
     name: 'Destroyer',
     size: 2,
